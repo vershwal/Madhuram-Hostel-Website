bb7bbaa38d1a3d3f15ddf605a84a28a7667e1d54
Paras Verma <paras@pop-os.localdomain>
2020-07-14 01:25:34 +0530

    Hero-Footer Text + BG Hero Outline Padding Image Caro Gallery Popup Caro Backup

--- js/main.js
+++ js/main.js
@@ -124,25 +124,12 @@ jQuery(document).ready(function($) {
 
 
 	var siteMagnificPopup = function() {
-		$('.image-popup').magnificPopup({
-	    type: 'image',
-	    closeOnContentClick: true,
-	    closeBtnInside: false,
-	    fixedContentPos: true,
-	    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-	     gallery: {
-	      enabled: true,
-	      navigateByImgClick: true,
-	      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-	    },
-	    image: {
-	      verticalFit: true
-	    },
-	    zoom: {
-	      enabled: true,
-	      duration: 300 // don't foget to change the duration also in CSS
-	    }
-	  });
+		$('.single-popup').magnificPopup({ 
+			type: 'image',
+			removalDelay: 300,
+			mainClass: 'mfp-with-fade',
+			closeOnContentClick: true
+		  });
 
 	  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
 	    disableOn: 700,
@@ -328,5 +315,6 @@ jQuery(document).ready(function($) {
 
 	}
 	windowScrolled();
+
 
 });
