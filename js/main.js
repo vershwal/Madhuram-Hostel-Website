f0202c309c0b01e52d4afcc24868685fc1502cf0
Paras Verma <paras@pop-os.localdomain>
2020-07-14 01:48:25 +0530

    JS For Single Image Pop-up Hero-Footer Text Option Added

--- js/main.js
+++ js/main.js
@@ -124,12 +124,26 @@ jQuery(document).ready(function($) {
 
 
 	var siteMagnificPopup = function() {
-		$('.single-popup').magnificPopup({ 
+		$('.image-popup').magnificPopup({
 			type: 'image',
-			removalDelay: 300,
-			mainClass: 'mfp-with-fade',
-			closeOnContentClick: true
+			closeOnContentClick: true,
+			closeBtnInside: false,
+			fixedContentPos: true,
+			mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+			 gallery: {
+			  enabled: true,
+			  navigateByImgClick: true,
+			  preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			},
+			image: {
+			  verticalFit: true
+			},
+			zoom: {
+			  enabled: true,
+			  duration: 300 // don't foget to change the duration also in CSS
+			}
 		  });
+		
 
 	  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
 	    disableOn: 700,
@@ -142,6 +156,17 @@ jQuery(document).ready(function($) {
 	  });
 	};
 	siteMagnificPopup();
+
+
+	var singleImageCaro = function () {
+		$('.single-popup').magnificPopup({ 
+		type: 'image',
+		removalDelay: 300,
+		mainClass: 'mfp-with-fade',
+		closeOnContentClick: true
+	  });
+	};
+	singleImageCaro();
 
 
 	var siteCarousel = function () {
